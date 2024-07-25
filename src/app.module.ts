772a5c84a5e91021b367b8c3790278a92fc282da
leeyeonwoo <rexrex1105@gmail.com>
2024-07-25 11:05:26 +0900

    chore: save commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArticleService } from './article/article.service';
+import { ArticleModule } from './article/article.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [ArticleModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ArticleService],
 })
 export class AppModule {}
