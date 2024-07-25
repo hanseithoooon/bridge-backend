cce1fe1ddecc45f3c7859428e3e3f69fbd8d3083
leeyeonwoo <rexrex1105@gmail.com>
2024-07-25 13:36:26 +0900

    feat: add global env settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 import { PostService } from './post/post.service';
 import { ArticleModule } from './post/post.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ArticleModule, PrismaModule, AuthModule, UserModule],
+  imports: [
+    ArticleModule,
+    PrismaModule,
+    AuthModule,
+    UserModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   providers: [AppService, PostService],
 })
 export class AppModule {}
